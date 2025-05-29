c914115353ee461f64a5604a02d7ffa542a9a285
alessio <alessio.taruffi2020@gmail.com>
2025-05-29 15:01:40 +0200

    pulsante avanti

---
+++ app/games/gamesList.tsx
@@ -0,0 +1,10 @@
+export const gamesList = [
+  "/games/minigame1",
+  "/games/minigame2",
+  "/games/minigamegolf",
+  "/games/minigamememo",
+  "/games/minigameTorre",
+  "/games/minigameShot",
+  "/games/minigameLigth",
+  "/games/minigameConta"
+];
